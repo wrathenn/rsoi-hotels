e6ca73f11df0e78db7ec5744962cfe7f930f926a
Rostislav <shatskii.re@perfema.com>
2024-07-17 21:22:22 +0300

    asdf

--- hotel-db/manage.sh
+++ hotel-db/manage.sh
@@ -6,7 +6,7 @@ until pg_isready --quiet --timeout=1; do sleep 1; done
 
 export PGDATABASE=postgres
 
-psql --set=ON_ERROR_STOP=1 --file=/init/00_users.sql
-psql --set=ON_ERROR_STOP=1 --file=/init/01_reservation.sql
-psql --set=ON_ERROR_STOP=1 --file=/init/02_payment.sql
-psql --set=ON_ERROR_STOP=1 --file=/init/03_loyalty.sql
+psql --set=ON_ERROR_STOP=1 --file=/hotel-db/init/00_users.sql
+psql --set=ON_ERROR_STOP=1 --file=/hotel-db/init/01_reservation.sql
+psql --set=ON_ERROR_STOP=1 --file=/hotel-db/init/02_payment.sql
+psql --set=ON_ERROR_STOP=1 --file=/hotel-db/init/03_loyalty.sql
