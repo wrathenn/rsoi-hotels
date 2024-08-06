0453525e59d5da0c72881e860d72e62d3a3273a4
Rostislav <shatskii.re@perfema.com>
2024-08-07 01:01:18 +0300

    why empty

--- backend/gateway-service/src/main/kotlin/com/wrathenn/gateway/service/clients/StatsClient.kt
+++ backend/gateway-service/src/main/kotlin/com/wrathenn/gateway/service/clients/StatsClient.kt
@@ -1,6 +1,7 @@
 package com.wrathenn.gateway.service.clients
 
 import com.wrathenn.util.models.statistics.Stat
+import org.slf4j.LoggerFactory
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Component
@@ -19,7 +20,10 @@ interface StatsClient {
 
 @Component
 class StatsClientFallback: StatsClient {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     override fun getStats(): List<Stat<*>> {
+        logger.warn("Unsuccessful getStats, using empty list")
         return emptyList()
     }
 }
